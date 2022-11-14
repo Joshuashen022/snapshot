568f41597d84e4247573b0a3f81830eba5f792b2
Joshua Shen <joshua183@asymmfund.com>
2022-11-14 13:03:45 +0800

    updates

--- src/crypto/format.rs
+++ src/crypto/format.rs
@@ -1,6 +1,8 @@
 use ordered_float::OrderedFloat;
 use std::collections::BTreeMap;
-use serde::Deserialize;
+use std::fmt;
+use serde::{Deserialize, Deserializer};
+use serde::de::{SeqAccess, Visitor};
 use crate::Quote;
 
 pub struct Shared {
@@ -42,6 +44,88 @@ pub struct Event{
 
 #[derive(Deserialize, Debug)]
 pub struct Data{
-    pub bids: Vec<[String;3]>,
-    pub asks: Vec<[String;3]>,
+    pub bids: Vec<Quotes>,
+    pub asks: Vec<Quotes>,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct Quotes{
+    price: f64,
+    amount: f64,
+    order_numbers: u64,
+}
+
+impl<'de> Deserialize<'de> for Quotes {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: Deserializer<'de>,
+    {
+        deserializer.deserialize_tuple(3, QuotesVisitor)
+    }
+}
+
+struct QuotesVisitor;
+
+impl<'de> Visitor<'de> for QuotesVisitor {
+    type Value = Quotes;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "a map with keys 'first' and 'second'")
+    }
+
+    fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+        where
+            A: SeqAccess<'de>,
+    {
+        let mut price = None;
+        let mut amount = None;
+        let mut order_numbers = None;
+
+        if let Some(val) = seq.next_element::<&str>()? {
+            match val.parse::<f64>() {
+                Ok(num) => price = Some(num),
+                Err(_) => return Err(serde::de::Error::custom("Fail to convert price str to f64")),
+            }
+        }
+
+        if let Some(val) = seq.next_element::<&str>()? {
+            match val.parse::<f64>() {
+                Ok(num) => amount = Some(num),
+                Err(_) => {
+                    return Err(serde::de::Error::custom(
+                        "Fail to convert amount str to f64",
+                    ))
+                }
+            }
+        }
+
+        if let Some(val) = seq.next_element::<&str>()? {
+            match val.parse::<u64>() {
+                Ok(num) => order_numbers = Some(num),
+                Err(_) => {
+                    return Err(serde::de::Error::custom(
+                        "Fail to convert order_numbers str to u64",
+                    ))
+                }
+            }
+        }
+
+        if price.is_none() {
+            return Err(serde::de::Error::custom("Missing price field"));
+        }
+
+        if amount.is_none() {
+            return Err(serde::de::Error::custom("Missing amount field"));
+        }
+
+        if order_numbers.is_none() {
+            return Err(serde::de::Error::custom("Missing order_numbers field"));
+        }
+
+        Ok(Quotes {
+            price: price.unwrap(),
+            amount: amount.unwrap(),
+            order_numbers: order_numbers.unwrap(),
+        })
+    }
 }
