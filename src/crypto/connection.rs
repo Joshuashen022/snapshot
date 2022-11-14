d8d6872ecd711e10848f1d8106f953e7c2525bc5
Joshua Shen <joshua183@asymmfund.com>
2022-11-14 14:17:02 +0800

    update

--- src/crypto/connection.rs
+++ src/crypto/connection.rs
@@ -40,9 +40,6 @@ impl CryptoOrderBookSpot {
                         (*guard) = false;
                     }
 
-                    info!("decoding level_event");
-                    let text = reqwest::get(&level_address).await?.text().await?;
-                    info!("get {}", text);
                     let level_event: LevelEventStream = reqwest::get(&level_address).await?.json().await?;
 
                     let level_event = level_event.result;
