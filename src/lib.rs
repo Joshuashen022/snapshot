f7466bc19238c495f1fa37692f04906426eeab85
Joshua Shen <joshua183@asymmfund.com>
2022-11-14 18:09:26 +0800

    shell push

--- src/lib.rs
+++ src/lib.rs
@@ -248,14 +248,12 @@ mod tests {
 
         let depths:Vec<Depth> = buffer1.split("\n").collect::<Vec<_>>().iter()
             .map(|&data|{
-                let data = format!("{}", data);
-                serde_json::from_str(&data.to_string()).unwrap()
+                serde_json::from_str(&(data.to_string())).unwrap()
             })
             .collect();
         let depth_levels:Vec<Depth> = buffer2.split("\n").collect::<Vec<_>>().iter()
             .map(|&data|{
-                let data = format!("{}", data);
-                serde_json::from_str(&data.to_string()).unwrap()
+                serde_json::from_str(&(data.to_string())).unwrap()
             })
             .collect();
 
